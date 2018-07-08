afe270f57da112ef7eb0c3fc0a6ca7e451b6911f
al315165 <33588283+al315165@users.noreply.github.com>
2018-07-08 02:01:11 +0200

    Trigger Event

---
+++ ArtVar/Assets/Scripts/triggerEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+//using UnityEngine.EventSystems;
+[System.Serializable]
+
+public class triggerEvent : MonoBehaviour {
+	public GameObject music;
+	public int variableInt=0;
+//	public Collider collider;
+//	[SerializeField] private float m_MaxSpeed = 10f;    
+
+//	// Use this for initialization
+//	void Start () {
+//		collider = GameComponent<BoxCollider2D> ();
+//
+//	}
+	
+	// Update is called once per frame
+//	void Update () {
+//		if (GetComponent<BoxCollider2D>().isTrigger) {
+//			variableInt += 10;
+//
+//			//music.SetActive (true);
+//		}
+//		
+//	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		variableInt += 10;
+		music.SetActive (true);
+	}
+}
