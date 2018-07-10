43e8ba85fcc48e7e99a07a51dcfd9f8b323801b9
al315165 <33588283+al315165@users.noreply.github.com>
2018-07-11 00:57:11 +0200

    Dying Event

---
+++ ArtVar/Assets/Scripts/DyingEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DyingEvent : MonoBehaviour {
+	public GameObject gameover;
+	public GameObject player;
+	public float y;
+	// Use this for initialization
+	void Start () {
+		
+//		player = GameObject.Find ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		y = transform.position.y;
+		if (y <= -12f) 
+		{
+			player.SetActive (false);
+			gameover.SetActive (true);
+		}
+		
+	}
+}
