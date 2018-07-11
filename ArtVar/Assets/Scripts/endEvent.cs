86d090057907fdaea37aedb9b099706f2b58685f
al315165 <33588283+al315165@users.noreply.github.com>
2018-07-12 00:46:26 +0200

    Ready to merge

---
+++ ArtVar/Assets/Scripts/endEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endEvent : MonoBehaviour {
+	public GameObject end; 
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		
+		end.SetActive (true);
+	}
+
+}
