7d41217ce291fb76ba23d9437c025cc613bf199e
Artemis Davar <artemisdavar@yahoo.com>
2018-07-13 18:36:44 -0500

    Spike

---
+++ ArtVar/Assets/Scripts/urded.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+[System.Serializable]
+
+public class urded : MonoBehaviour {
+    public GameObject player;
+	public GameObject GameOver;
+	
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		player.SetActive (false);
+		GameOver.SetActive(true);
+	}
+}
