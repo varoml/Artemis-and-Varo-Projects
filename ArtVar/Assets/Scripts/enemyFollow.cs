c459dfc9a0037af369fb5e16f940ea33b3c30920
al315165 <33588283+al315165@users.noreply.github.com>
2018-07-16 02:52:15 +0200

    EnemyAI

---
+++ ArtVar/Assets/Scripts/enemyFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyFollow : MonoBehaviour {
+	public float speed;
+	private Transform target;
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x, transform.position.y, transform.position.z) , speed * Time.deltaTime);
+	}
+}
